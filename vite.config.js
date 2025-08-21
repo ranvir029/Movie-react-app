4ce1f6a014a6a39a43ab7aff8da81fe3d6406c8c
Ranvir <uk845816@gmail.com>
2025-08-21 12:03:14 +0530

    second commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   optimizeDeps: {
+    include: ['bootstrap'] // ✅ helps Vite pre-bundle bootstrap
+  },
+   build: {
+    rollupOptions: {
+      external: []
+    }}
 })
